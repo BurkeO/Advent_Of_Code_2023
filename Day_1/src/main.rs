0ac48f46d5754eca1d7d4e1478fbd4202ec5ffc7
Owen Burke <35539701+BurkeO@users.noreply.github.com>
2023-12-02 20:26:15 +1100

    working

--- Day_1/src/main.rs
+++ Day_1/src/main.rs
@@ -1,9 +1,18 @@
 use phf::phf_map;
-use std::{fs, str::Chars};
+use std::fs;
 
 static INPUT_FILE: &str = "input.txt";
 
 static DIGIT_STRINGS_TO_INT_MAP: phf::Map<&str, u32> = phf_map! {
+    "1" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
     "one" => 1,
     "two" => 2,
     "three" => 3,
@@ -15,102 +24,43 @@ static DIGIT_STRINGS_TO_INT_MAP: phf::Map<&str, u32> = phf_map! {
     "nine" => 9
 };
 
-fn find_pos_and_value_of_digit_char(iter: Chars) -> Option<(usize, u32)> {
-    iter.enumerate()
-        .find(|(_, char)| char.is_numeric())
-        .map(|(index, char)| (index, char.to_digit(10).unwrap()))
-}
-
 fn first_and_last_digit_as_num(input_string: &str) -> Option<u32> {
-    let first_digit_position_value_opt = find_pos_and_value_of_digit_char(input_string.chars());
-    let mut last_digit_position_value_opt =
-        find_pos_and_value_of_digit_char(input_string.chars().rev().collect::<String>().chars());
-    
-    if last_digit_position_value_opt.is_some() {
-        last_digit_position_value_opt = Some((
-            input_string.len() - 1 - last_digit_position_value_opt.unwrap().0,
-            last_digit_position_value_opt.unwrap().1,
-        ));
-    }
-
     let mut first_digit_string_position_value_opt: Option<(usize, &u32)> = None;
     let mut last_digit_string_position_value_opt: Option<(usize, &u32)> = None;
 
     for (digit_string, digit_int) in DIGIT_STRINGS_TO_INT_MAP.into_iter() {
-        if let Some(position) = input_string.find(digit_string) {
+        let first_position = input_string.find(digit_string);
+        let last_position = input_string.rfind(digit_string);
+
+        if let Some(first_pos) = first_position {
             if let Some((first_digit_string_pos, _)) = first_digit_string_position_value_opt {
-                first_digit_string_position_value_opt = if position < first_digit_string_pos {
-                    Some((position, digit_int))
+                first_digit_string_position_value_opt = if first_pos < first_digit_string_pos {
+                    Some((first_pos, digit_int))
                 } else {
                     first_digit_string_position_value_opt
                 }
+            } else {
+                first_digit_string_position_value_opt = Some((first_pos, digit_int))
             }
-            else {
-                first_digit_string_position_value_opt = Some((position, digit_int))
-            }
+        }
+
+        if let Some(last_pos) = last_position {
             if let Some((last_digit_string_pos, _)) = last_digit_string_position_value_opt {
-                last_digit_string_position_value_opt = if position > last_digit_string_pos {
-                    Some((position, digit_int))
+                last_digit_string_position_value_opt = if last_pos > last_digit_string_pos {
+                    Some((last_pos, digit_int))
                 } else {
                     last_digit_string_position_value_opt
                 }
-            }
-            else {
-                last_digit_string_position_value_opt = Some((position, digit_int))
-            }
-        }
-    }
-
-    let mut first_digit_opt: Option<u32> = None;
-    let mut last_digit_opt: Option<u32> = None;
-
-    match (
-        first_digit_position_value_opt,
-        first_digit_string_position_value_opt,
-    ) {
-        (Some((digit_pos, digit)), Some((digit_string_pos, digit_int))) => {
-            if digit_pos < digit_string_pos {
-                first_digit_opt = Some(digit);
             } else {
-                first_digit_opt = Some(*digit_int);
+                last_digit_string_position_value_opt = Some((last_pos, digit_int))
             }
         }
-        (Some((_, digit)), None) => {
-            first_digit_opt = Some(digit);
-        }
-        (None, Some((_, digit))) => {
-            first_digit_opt = Some(*digit);
-        }
-        (None, None) => return None,
     }
 
-    match (
-        last_digit_position_value_opt,
-        last_digit_string_position_value_opt,
-    ) {
-        (Some((digit_pos, digit)), Some((digit_string_pos, digit_int))) => {
-            if digit_pos > digit_string_pos {
-                last_digit_opt = Some(digit);
-            } else {
-                last_digit_opt = Some(*digit_int);
-            }
-        }
-        (Some((_, digit)), None) => {
-            last_digit_opt = Some(digit);
-        }
-        (None, Some((_, digit))) => {
-            last_digit_opt = Some(*digit);
-        }
-        (None, None) => return None,
-    }
-
-
-    let val = first_digit_opt.map(|first_digit| first_digit * 10 + last_digit_opt.unwrap());
-    println!("{}", val.unwrap());
-    val
+    let val = first_digit_string_position_value_opt.unwrap().1 * 10
+        + last_digit_string_position_value_opt.unwrap().1;
+    Some(val)
 }
-
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_as_string =
